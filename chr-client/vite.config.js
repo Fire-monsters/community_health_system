c1ca260e429c70b5b8bbb949d7c112f105c912e4
Fire-monsters <kalibobojoshua@gmail.com>
2026-05-18 06:34:48 +0300

    fixing ngrok

--- chr-client/vite.config.js
+++ chr-client/vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+
 export default defineConfig({
+    server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    },
+    allowedHosts: [
+      'jona-unwadded-selfishly.ngrok-free.dev'
+    ]
+  },
   plugins: [
     react(),
     VitePWA({
